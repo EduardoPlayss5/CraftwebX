3dba151c1247ba023c3204e83ccacdfe299d8aa1
EduardoPlayss5 <139657857+EduardoPlayss5@users.noreply.github.com>
2023-10-21 19:45:18 -0300

    Add files via upload

---
+++ games/eaglercraft-opts.js
@@ -0,0 +1,10 @@
+window.eaglercraftXOpts = () => ({
+	lang: "pt_BR",
+  // joinServer: "wss://reading-gs.q13x.com",
+  // assetsURI: "custom.epk",
+	servers: [
+		{ addr: "wss://reading-gs.q13x.com", name: "q13x Public Server" }
+	]
+});
+// document.title = "My Original Server Name:tm:";
+// document.querySelector("link[rel=\"shortcut icon\"]").href = "https://www.google.com/favicon.ico";
